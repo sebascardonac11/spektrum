359a8b73221976e6ce3d197c24b1a3c9ad579b0d
Sebastian Cardona Correa <placebo@192.168.1.12>
2022-04-28 11:16:45 -0500

    funcionando el MCP008

--- spektrum.ino
+++ spektrum.ino
@@ -1,10 +1,19 @@
 #include <ESP8266WiFi.h>
 #include <Wire.h>
 #include <Ticker.h>
+#include <MCP3008.h>
 #include "spektrum_xbus.h"
 
 #define ledAzul D4 //Led Azul de la placa
 #define ledRojo 16 // Led Rojo de la placa#define D0 16 // Led Rojo de la placa
+// define pin connections MCP
+#define CS_PIN D8
+#define CLOCK_PIN D5
+#define MOSI_PIN D7
+#define MISO_PIN D6
+
+// put pins inside MCP3008 constructor
+MCP3008 adc(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
 
 const uint8_t spek_data_size = 16;
 byte spek_data_1[spek_data_size];
@@ -16,7 +25,6 @@ void setup() {
   pinMode(ledAzul,OUTPUT); //Led Azul de la placa
   pinMode(ledRojo,OUTPUT);// Led rojo de la placa
   digitalWrite(ledRojo,HIGH);
-
   //Begin I2C setup
   //setupI2C();
 }
@@ -52,8 +60,19 @@ void i2cRequestEvent(){
 }
 
 void readCells(){
+  int val = adc.readADC(0); // read Chanel 0 from MCP3008 ADC
+  
   float voltage=0;
   voltage = (analogRead(A0)/1024.0)*25.0;
+  Serial.println("");
   Serial.print("Voltage : ");
   Serial.println(voltage);
-}
+  for (int i=0; i<8; i++) {
+    int val = adc.readADC(i);
+     Serial.print("(");
+    Serial.print(i);
+    Serial.print(")");
+    Serial.print((val/1024.0)*5.0);
+    Serial.print("\t");
+    }
+   }
