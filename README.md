c2f4b4d4a05954ebc91ef4cb3a265be6a1ef7755
Sebastian Cardona Correa <placebo@192.168.1.12>
2022-05-02 17:40:54 -0500

    MCP3008 funcionando

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Todo esto es posible gracias a esta guia
 https://www.spektrumrc.com/ProdInfo/Files/SPM_Telemetry_Developers_Specs.pdf
 
 Spektrum uses a JST-XH size 4-pin male connector on the receiver for the "Xbus" port
+https://www.digikey.es/es/products/detail/jst-sales-america-inc/04JQ-BT/4918835?s=N4IgTCBcDaIAwBYBSBFAtAIQCogLoF8g
 
 Conversor Analogo a Digital
 https://octopart.com/datasheet/mcp3008-i%2Fp-microchip-407339
@@ -14,5 +15,8 @@ https://github.com/makersdigest/T04-MCP3008
 
 Guia de Altimetro
 https://www.instructables.com/RC-Plane-Altimeter-compatible-With-Spektrum-Teleme/
+
+Calculadora de Divisor de Voltaje Online
+https://mielectronicafacil.com/herramienta/calculadora-divisor-de-voltaje/#page-content
 
 Phrase: Daytona
--- spektrum.ino
+++ spektrum.ino
@@ -15,10 +15,16 @@
 // put pins inside MCP3008 constructor
 MCP3008 adc(CLOCK_PIN, MOSI_PIN, MISO_PIN, CS_PIN);
 
+uint16_t lipoCell[6]={3.7,3.7,3.7,3.7,3.7,3.7};
 const uint8_t spek_data_size = 16;
 byte spek_data_1[spek_data_size];
 byte spek_data_2[spek_data_size];
 bool write_buff_1 = true;
+byte * buff_ptr;
+STRU_TELE_LIPOMON alt_struct_Lipo;
+
+
+float Vref[]= {4.13,5.21,5.0,4.23,5.0,4.35,4.10,4.14}; 
 
 void setup() {
   Serial.begin(115200);
@@ -26,12 +32,17 @@ void setup() {
   pinMode(ledRojo,OUTPUT);// Led rojo de la placa
   digitalWrite(ledRojo,HIGH);
   //Begin I2C setup
-  //setupI2C();
+  alt_struct_Lipo.identifier = TELE_DEVICE_LIPOMON;
+  alt_struct_Lipo.sID=0;
+  memcpy(alt_struct_Lipo.cell,lipoCell,6);
+  alt_struct_Lipo.temp=0;
+  setupI2C();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   readCells();
+  memcpy(spek_data_1, &alt_struct_Lipo, sizeof(alt_struct_Lipo));
 
   digitalWrite(ledAzul,HIGH);
   delay(500);
@@ -41,38 +52,35 @@ void loop() {
 void setupI2C(){
 
   // Start the client I2C device
+   Wire.begin(TELE_DEVICE_LIPOMON);
   Wire.onRequest(i2cRequestEvent);
-  Wire.begin(TELE_DEVICE_LIPOMON);
+ 
 
 }
 void i2cRequestEvent(){
 
-  // Write the LipoInfo data to the I2C bus
-  if (write_buff_1){
-    Wire.write(spek_data_2, 16);
-  } else {
-    Wire.write(spek_data_1, 16);
-  }
-
-  // Alternate the LED to show there are I2C events
-  digitalWrite(ledAzul, write_buff_1);
-  
+  Wire.write(spek_data_1, 16);
+  Serial.println("Tamano ");
+  Serial.print(sizeof(alt_struct_Lipo));
 }
 
 void readCells(){
   int val = adc.readADC(0); // read Chanel 0 from MCP3008 ADC
-  
   float voltage=0;
   voltage = (analogRead(A0)/1024.0)*25.0;
-  Serial.println("");
-  Serial.print("Voltage : ");
-  Serial.println(voltage);
-  for (int i=0; i<8; i++) {
-    int val = adc.readADC(i);
+  alt_struct_Lipo.cell[0]=(adc.readADC(0)/1024.0)*Vref[0];
+  alt_struct_Lipo.cell[1]=(adc.readADC(1)/1024.0)*Vref[1];
+  alt_struct_Lipo.cell[2]=(adc.readADC(3)/1024.0)*Vref[3];
+  alt_struct_Lipo.cell[3]=(adc.readADC(5)/1024.0)*Vref[5];
+  alt_struct_Lipo.cell[4]=(adc.readADC(6)/1024.0)*Vref[6];
+  alt_struct_Lipo.cell[5]=(adc.readADC(7)/1024.0)*Vref[7];
+Serial.print("cells ");
+  for (int i=0; i<6; i++) {
      Serial.print("(");
     Serial.print(i);
     Serial.print(")");
-    Serial.print((val/1024.0)*5.0);
+    Serial.print(alt_struct_Lipo.cell[i]);
     Serial.print("\t");
     }
+    Serial.print("\n");
    }
